3c3ee919919e78a87cfbfe038aa7434f050968f3
berywhite <jakubbereta@gmail.com>
2026-06-24 20:18:10 +0200

    config: Supabase URL i anon key

--- supabase_config.js
+++ supabase_config.js
@@ -6,5 +6,5 @@
 // 3. Skopiuj "Project URL" i "anon public" key poniżej
 // 4. Uruchom SQL z pliku supabase_schema.sql w SQL Editor
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL_HERE';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://imzsuzkwfqukyujmwknj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltenN1emt3ZnF1a3l1am13a25qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMTg1NDEsImV4cCI6MjA5Nzg5NDU0MX0.G11b_n0JvTr4LsJ-Ye65MLMwCs48TB4zebUX2cURFn0';
